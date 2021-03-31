6b8662e6c6441cd579a91be9482c1132c55c21d5
xrdnk <xrdnk1010@gmail.com>
2021-03-31 09:40:09 +0900

    style: added comment

--- Assets/MLAPIQuickStart/Scripts/View/ConnectionView.cs
+++ Assets/MLAPIQuickStart/Scripts/View/ConnectionView.cs
@@ -32,6 +32,7 @@ namespace MLAPIQuickStart.View
 
             buttonLeave.interactable = false;
 
+            // InputField に値が設定されるまで，ボタンを非活性にする
             inputFieldPassword.ObserveEveryValueChanged(inputField => inputField.text)
                 .Subscribe(text =>
                 {
@@ -52,13 +53,14 @@ namespace MLAPIQuickStart.View
 
         private void Host()
         {
+            // サーバ側（ここではホスト側）は接続許可設定を行う
             NetworkManager.Singleton.ConnectionApprovalCallback += ApprovalCheck;
             NetworkManager.Singleton.StartHost(new Vector3(-2f, 0f, 0f), Quaternion.Euler(0f, 135f, 0f));
         }
 
         private void Client()
         {
-            // Set password ready to send to the server to validate
+            // クライアント側はサーバ側へ接続時に必要なデータを送る必要がある
             NetworkManager.Singleton.NetworkConfig.ConnectionData = Encoding.ASCII.GetBytes(inputFieldPassword.text);
             NetworkManager.Singleton.StartClient();
         }
@@ -105,34 +107,27 @@ namespace MLAPIQuickStart.View
         /// <summary>
         /// 接続チェック用のコールバック
         /// </summary>
-        /// <param name="connectionData">ClientがServerに送信する任意のカスタムデータ</param>
+        /// <param name="connectionData">ペイロード</param>
         /// <param name="clientId">ClientId</param>
         /// <param name="callback">デリゲート</param>
-        private void ApprovalCheck(byte[] connectionData, ulong clientId, NetworkManager.ConnectionApprovedDelegate callback)
+        private void ApprovalCheck(byte[] connectionData, ulong clientId,
+            NetworkManager.ConnectionApprovedDelegate callback)
         {
+            // クライアント側がサーバ側に送る時のペイロード
             var password = Encoding.ASCII.GetString(connectionData);
 
+            // 今回ここでは inputFieldPassword.text の値は「ホスト側」が設定した値になる
+            // なので，ホスト側とクライアント側のパスワードが一致した時に true になる
+            // ここで false になった場合，callback は発火せず，接続できない
             var approveConnection = password.Equals(inputFieldPassword.text);
 
-            var spawnPos = Vector3.zero;
+            var spawnPos = new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5));
             var spawnRot = Quaternion.identity;
-
-            switch (NetworkManager.Singleton.ConnectedClients.Count)
-            {
-                case 1:
-                    spawnPos = new Vector3(0f, 0f, 0f);
-                    spawnRot = Quaternion.Euler(0f, 180f, 0f);
-                    break;
-                case 2:
-                    spawnPos = new Vector3(2f, 0f, 0f);
-                    spawnRot = Quaternion.Euler(0f, 225, 0f);
-                    break;
-            }
 
             // 第一引数はデリゲートを通してプレイヤーオブジェクトを生成するか否か．個人でカスタマイズしたい場合は false にする．
             // 第二引数はどのプレハブを生成するか（ハッシュ値で）．"Default Player Prefab" にチェックが入っている場合は null にする．
             // 第三引数は接続の許可が得られたか否か．大方，パスワードが合っているかどうかの判定をここに入れる．脳死で通したい場合は true にする．
-            // 第四引数，第五引数は生成時にプレイヤーに渡すPositionとRotation．nullable なので デフォルトを渡したい場合は null にする．
+            // 第四引数，第五引数は生成時にプレイヤーに渡すPositionとRotation．プレハブのデフォルト値を渡したい場合は null にする．
             callback(true, null, approveConnection, spawnPos, spawnRot);
         }
     }
